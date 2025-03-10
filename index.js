11a8edc6e9553201382cc5dda123170dacf92adb
iqra <abdikarimiqra@gmail.com>
2025-03-10 23:53:30 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = function(num1, num2){
+    return num1/num2
+
+}
+divide(2000/100);
+
+const square = num=> {
+    return num * num
+}
+
+square(4 * 4);
+
+const add = (num3 , num4) =>{
+    return num3 + num4
+     
+}
+
+add (3,4);
